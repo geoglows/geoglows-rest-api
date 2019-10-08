fbc6d4dee5c66b7a756a9b1debda39f36decfe82
msouff <msouffront@aquaveo.com>
2019-10-08 13:45:06 -0600

    reverted from mounts back to azcopy

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 python /app/azcopy/file_mounter.py
 
 crontab -l > dailyforecastcron
-echo "0 3 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
+echo "0 9 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
 crontab dailyforecastcron
 rm dailyforecastcron
 
