f51d865a8e28c27d97ec067bc311bc7b14571fe0
Rohit Khattar <rohitkh@byu,edu>
2019-11-15 14:44:39 -0700

    Temp

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python /app/azcopy/file_mounter.py
+# python /app/azcopy/file_mounter.py
 
-crontab -l > dailyforecastcron
-echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
-crontab dailyforecastcron
-rm dailyforecastcron
+# crontab -l > dailyforecastcron
+# echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
+# crontab dailyforecastcron
+# rm dailyforecastcron
 
 /usr/bin/supervisord
