c1c785522c717e9766fb5aad2f8398347ec8b3ba
Rohit Khattar <rohitkh@byu,edu>
2019-11-25 16:38:15 -0700

    Fixed cron

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# python /app/azcopy/file_mounter.py
+python /app/azcopy/file_mounter.py
 
-# crontab -l > dailyforecastcron
-# echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
-# crontab dailyforecastcron
-# rm dailyforecastcron
+service cron start
+crontab -l > dailyforecastcron
+echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
+crontab dailyforecastcron
 
 /usr/bin/supervisord
