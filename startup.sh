8a21e57b7f5b25d67da69b4b2317cbac3df34c43
Rohit Khattar <rohitkh@byu,edu>
2019-11-25 21:49:25 -0700

    No need to mount for sub containers

--- startup.sh
+++ startup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-python /app/azcopy/file_mounter.py
 
-service cron start
-crontab -l > dailyforecastcron
-echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
-crontab dailyforecastcron
+FILE=/mnt/output
+if [ -d "$FILE" ]; then
+    echo "Output already exists. No need to re create it or any cron jobs"
+else
+	python /app/azcopy/file_mounter.py
+	service cron start
+	crontab -l > dailyforecastcron
+	echo "0 12 * * * /bin/bash /app/azcopy/forecast-workflow.sh" >> dailyforecastcron
+	crontab dailyforecastcron
+fi
 
 /usr/bin/supervisord
