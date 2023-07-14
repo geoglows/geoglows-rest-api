67d1b8748b411319494d1e9eaaa311c6ee63aec5
msouffront <17328135+msouff@users.noreply.github.com>
2023-07-14 14:49:56 -0600

    revert constants.py

--- GSP_API/constants.py
+++ GSP_API/constants.py
@@ -1,5 +1,5 @@
-PATH_TO_FORECASTS = '/home/michael/development/gsp_rest_api/sample_data/forecasts'  # '/mnt/output/forecasts'
+PATH_TO_FORECASTS = '/mnt/output/forecasts'
 PATH_TO_FORECAST_RECORDS = '/mnt/output/forecast-records'
 PATH_TO_ERA_INTERIM = '/mnt/output/era-interim'
-PATH_TO_ERA_5 = '/home/michael/development/gsp_rest_api/sample_data/era5'  # '/mnt/output/era-5'
+PATH_TO_ERA_5 = '/mnt/output/era-5'
 M3_TO_FT3 = 35.3146667
