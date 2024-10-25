964149d3d35acc1d846abcd0dad189a5cb8d3406
msouffront <17328135+msouff@users.noreply.github.com>
2024-10-24 20:14:25 -0600

    stop running uwsgi as root

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uwsgi --master --max-requests 1000 --max-worker-lifetime 1800 --worker-reload-mercy 30 --virtualenv="/opt/conda/envs/app-env" --http 0.0.0.0:80 -b 32768 --die-on-term --enable-threads --log-date="%Y-%m-%d %H:%M:%S" --logformat-strftime --processes=8 --wsgi-file="/app/app.py" --callable app 
+uwsgi --master --uid nobody --gid nogroup --max-requests 1000 --max-worker-lifetime 1800 --worker-reload-mercy 30 --virtualenv="/opt/conda/envs/app-env" --http 0.0.0.0:80 -b 32768 --die-on-term --enable-threads --log-date="%Y-%m-%d %H:%M:%S" --logformat-strftime --processes=8 --wsgi-file="/geoglows/app/app.py" --callable app 
