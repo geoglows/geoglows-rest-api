7f679c537e89f78c9de7f06f607fa931c7659613
rhuber <rhuber@aquaveo.com>
2025-02-13 08:55:39 -0700

    fixed table uri

--- app/v2/constants.py
+++ app/v2/constants.py
@@ -6,4 +6,7 @@ PATH_TO_RETURN_PERIODS = "/app/return-periods.zarr"
 PACKAGE_METADATA_TABLE_PATH = os.getenv(
     "PYGEOGLOWS_METADATA_TABLE_PATH", "/app/package-metadata-table.parquet"
 )
+PYGEOGLOWS_TRANSFORMER_TABLE_URI = os.getenv(
+    "PYGEOGLOWS_TRANSFORMER_TABLE_URI", "/app/transformer_table.parquet"
+)
 NUM_DECIMALS = 1
